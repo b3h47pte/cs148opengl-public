92fc83e33076f7e2dd9ca7dcd16807905a6ea815
Ed Quigley <ed.quigley@comcast.net>
2018-09-25 14:20:44 -0700

    update year

--- common/MediaLayer.cpp
+++ common/MediaLayer.cpp
@@ -48,7 +48,7 @@ void MediaLayer::InitializeSDL()
     SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 16);
 
     glm::vec2 windowSize(app->GetWindowSize());
-    sdlWindow = SDL_CreateWindow("CS148 Fall 2015-2016", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+    sdlWindow = SDL_CreateWindow("CS148 Fall 2018-2019", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         windowSize.x, windowSize.y, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
 
     if (!sdlWindow) {
